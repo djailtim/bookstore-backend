62d98e5967185e6c17abc811b38ab1754fb78f9f
Ailton Ferreira Luz <djailtim@gmail.com>
2023-06-19 15:02:16 -0300

    instancia automatica da base de dados

---
+++ src/main/java/com/d3lt4/bookstore/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.d3lt4.bookstore.repositories;
+
+import com.d3lt4.bookstore.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+}
---
+++ src/main/java/com/d3lt4/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.d3lt4.bookstore.repositories;
+
+import com.d3lt4.bookstore.domain.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+}
