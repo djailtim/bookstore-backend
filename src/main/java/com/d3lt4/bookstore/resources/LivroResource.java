74aef29a5b132f3d95a1bf44071c9138bbc92254
Ailton Ferreira Luz <djailtim@gmail.com>
2023-07-10 21:38:49 -0300

    refactor

--- src/main/java/com/d3lt4/bookstore/resources/LivroResource.java
+++ src/main/java/com/d3lt4/bookstore/resources/LivroResource.java
@@ -1,2 +1,23 @@
-package com.d3lt4.bookstore.resources;public class LivroResource {
+package com.d3lt4.bookstore.resources;
+
+import com.d3lt4.bookstore.domain.Livro;
+import com.d3lt4.bookstore.service.LivroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/livros")
+public class LivroResource {
+    @Autowired
+    private LivroService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Livro> findById (@PathVariable Integer id) {
+        Livro livro = service.findById(id);
+        return ResponseEntity.ok().body(livro);
+    }
 }
--- src/main/java/com/d3lt4/bookstore/service/LivroService.java
+++ src/main/java/com/d3lt4/bookstore/service/LivroService.java
@@ -1,2 +1,20 @@
-package com.d3lt4.bookstore.service;public class LivroService {
+package com.d3lt4.bookstore.service;
+
+import com.d3lt4.bookstore.domain.Categoria;
+import com.d3lt4.bookstore.domain.Livro;
+import com.d3lt4.bookstore.repositories.LivroRepository;
+import com.d3lt4.bookstore.service.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LivroService {
+    @Autowired
+    private LivroRepository repository;
+
+    public Livro findById(Integer id) {
+        return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException(
+                "Object not found, id: " + id + ". Tipo: " + Livro.class.getName()
+        ));
+    }
 }
